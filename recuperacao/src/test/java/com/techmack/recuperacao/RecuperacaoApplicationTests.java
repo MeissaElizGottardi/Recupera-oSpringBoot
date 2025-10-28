d3fe23032bdcb200bad700edf829f007c6b48fbf
Your Name <you@example.com>
2025-10-28 19:33:31 -0300

    Primeiro commit

---
+++ recuperacao/src/test/java/com/techmack/recuperacao/RecuperacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmack.recuperacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuperacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
